b70a52a49e9649fa93edeca45be3574d7f43b45d
RogelRC <rogelprogrammer@gmail.com>
2025-01-21 18:22:18 -0500

    Working on the top sells

--- src/components/Topsells.tsx
+++ src/components/Topsells.tsx
@@ -1,9 +1,21 @@
 function Topsells() {
     return (
-        <div className="flex h-[90vh] w-full bg-[#022953] relative">
-            <div className="flex h-full w-[50vw] bg-amber-950"></div>
-            <div className="absolute top-[10%] bottom-[10%] left-1/2 w-[1px] bg-white"></div>
-            <div className="flex h-full w-[50vw]"></div>
+        <div className="flex flex-col h-[88vh] w-full bg-[#022953] relative">
+            <div className="flex h-[15vh] w-full bg-amber-300 px-10 items-end">
+                <h1 className="font-bold text-5xl">Productos</h1>
+                <div className="ml-auto flex items-center space-x-2">
+                    <h3 className="font-bold text-lg">Ver más</h3>
+                    <span className="font-bold text-xl">{'>'}</span>
+                </div>
+            </div>
+
+            <div className="flex w-full h-full">
+                <div className="flex h-full w-[50vw] bg-amber-950">
+                </div>
+                <div className="absolute top-[10%] bottom-[10%] left-1/2 w-[1px] bg-white" /> {/* la línea del medio */}
+                <div className="flex h-full w-[50vw]">
+                </div>
+            </div>
         </div>
     );
 }
